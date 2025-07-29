8d3022f0ba47107545e3d8a821fd85d383340303
Oleksandr Palilov <opalilovit@gmail.com>
2025-07-29 16:07:03 +0300

    test get http response

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json;
+  ({ message: 'Hello world!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT} it's ok`);
+});
