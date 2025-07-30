621d01c600d7d9e5301b1552c97768bd67219cbb
Oleksandr Palilov <opalilovit@gmail.com>
2025-07-30 14:21:59 +0300

    practice mongoDB

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllStudents, getStudentsById } from './services/students.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -24,6 +25,27 @@ export const startServer = () => {
   // GET
   app.get('/', (req, res) => {
     res.json({ message: 'Hello world!' });
+  });
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+    res.status(200).json({
+      data: students,
+    });
+  });
+  app.get('/students/:studentId', async (req, res, next) => {
+    const { studentId } = req.params;
+    const student = await getStudentsById(studentId);
+
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      data: student,
+    });
   });
 
   // Middleware error
