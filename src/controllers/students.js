de7106870137e8a2553352e1b6f57677dcb8acaa
Oleksandr Palilov <opalilovit@gmail.com>
2025-08-13 16:16:49 +0300

    create filter params

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -8,11 +8,20 @@ import {
 } from '../services/students.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getStudentsController = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortOrder, sortBy } = parseSortParams(req.query);
-  const students = await getAllStudents({ page, perPage, sortBy, sortOrder });
+  const filter = parseFilterParams(req.query);
+
+  const students = await getAllStudents({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
 
   res.status(200).json({
     status: 200,
